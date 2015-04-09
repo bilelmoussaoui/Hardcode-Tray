3304173679cf62e7dd961ad14f7ba246d9a3f729
Test User <tktest@example.com>
2015-04-09 19:39:59 +0200

    Handles problem when source and destination icons have different extensions

--- script.py
+++ script.py
@@ -1,63 +1,79 @@
 __author__ = "Bilal ELMOUSSAOUI"
 version = 0.1
 
-import os,pwd,platform,csv
+import os, pwd, platform, csv, subprocess, sys
+import cairosvg
 from gi.repository import Gtk
-global db_file,folder_icons,username,theme,default_icon_size
+#global db_file,username,theme,default_icon_size
+
+if os.geteuid() != 0:
+    sys.exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
 
 db_file = "db.csv"
-folder_icons = "/usr/share/icons"
 username = pwd.getpwuid( os.getuid() )[ 0 ]
 useros = platform.linux_distribution()
 useros = useros[0].strip('"')
+desktopenv = os.environ.get('DESKTOP_SESSION')
 theme = Gtk.IconTheme.get_default()
 default_icon_size = 22
-if useros == 'elementary OS' or useros == 'XFCE':
-	default_icon_size = 24
+if useros == 'elementary OS' or useros == 'Xubuntu':
+    default_icon_size = 24
 
 #Check if the directory exists
 def is_dir(dir):
-	return os.path.isdir(dir)
-
-#create a link from a list
-def do_link(ls,start=False):
-	return "/".join(ls)
+    return os.path.isdir(dir)
 
 #convert the csv file to a dictionnary 
 def csv_to_dic():
-	d = open(db_file)
-	content = d.readlines()
-	d.close()
-	dic = {}
-	infos = []
-	for i in range(len(content)):
-		infos.append(content[i].strip("\n").split(","))
-		infos[i][1] = infos[i][1].replace("{user_name}",username)
-		if infos[i][0] not in dic.keys() and is_dir("/"+infos[i][1]):
-			dic[infos[i][0]] = infos[i]
-	return dic
+    d = open(db_file)
+    content = d.readlines()
+    d.close()
+    dic = {}
+    infos = []
+    for i in range(len(content)):
+        infos.append(content[i].strip("\n").split(","))
+        infos[i][1] = infos[i][1].replace("{user_name}",username)
+        if infos[i][0] not in dic.keys() and is_dir("/"+infos[i][1]):
+            dic[infos[i][0]] = infos[i]
+    return dic
 
 def get_icons(app,apps):
-	i = 2
-	icons = [] 
-	while i < len(apps[app]):
-		icons.append(apps[app][i])
-		i+=1
-	return icons
+    i = 2
+    icons = [] 
+    while i < len(apps[app]):
+        icons.append(apps[app][i])
+        i+=1
+    return icons
 
 #Copy files..
 def copy_files():
-	apps = csv_to_dic()
-	for app in apps:
-		app_icons = get_icons(app,apps)
-		for icon in app_icons:
-			if theme.lookup_icon(icon, default_icon_size, 0):
-				o_folder = "/"+apps[app][1] #Original folder
-				i_folder = theme.lookup_icon(icon, default_icon_size, 0).get_filename()#Icons folder
-				os.system("sudo ln -fns "+i_folder + " "+o_folder)
+    apps = csv_to_dic()
+    for app in apps:
+        app_icons = get_icons(app,apps)
+        for icon in app_icons:
+            base_icon = os.path.splitext(icon)[0]
+            extension_orig = os.path.splitext(icon)[1]
+            theme_icon = theme.lookup_icon(base_icon, default_icon_size, 0)
+            if theme_icon:
+                filename = theme_icon.get_filename()
+                extension_theme = os.path.splitext(filename)[1]
+                o_file = "/"+apps[app][1]+"/"+icon #Output icon
+                print (o_file)
+                if extension_theme == extension_orig:
+                    subprocess.Popen(['ln', '-sf', filename, o_file])
+                    print("%s -- fixed using %s"%(app, filename))
+                elif extension_theme == '.svg' and extension_orig == '.png':
+                    with open(filename, 'r') as content_file:
+                        svg = content_file.read()
+                    fout = open(o_file,'wb')
+                    cairosvg.svg2png(bytestring=bytes(svg,'UTF-8'),write_to=fout)
+                    fout.close()
+                else:
+                    sys.exit('hardcoded file has to be svg or png. Other formats not supported yet')
+        
 
 #The message shown to the user
 print("Welcome to the tray icons hardcoder fixer! \n")
 print("Copying now..\n")
 copy_files()
-print("Done , Thank you for using the Hardcode-Tray fixer!")
+print("\nDone , Thank you for using the Hardcode-Tray fixer!")
