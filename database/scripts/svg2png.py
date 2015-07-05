3b7c845a8bc9c442dca50150e635cc60af51cc6c
Test User <tktest@example.com>
2015-07-05 11:09:02 +0200

    Removes dependency for inkscape in chrome script

---
+++ database/scripts/svg2png.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from cairosvg import svg2png
+import sys
+
+origfile = sys.argv[1]
+symlfile = sys.argv[2]
+
+with open(origfile, 'r') as content_file:
+    svg = content_file.read()
+    fout = open(symlfile, 'wb')
+    svg2png(bytestring=bytes(svg, 'UTF-8'), write_to=fout)
+    fout.close()
