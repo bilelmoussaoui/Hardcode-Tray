c972d2d1bc532d56a475a4ede8ed1a5e13458845
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2015-08-12 23:27:00 +0200

    Fixed a small issue in dropbox icon detecting

--- script.py
+++ script.py
@@ -68,7 +68,7 @@ def get_subdirs(directory):
         dirs = listdir(directory)
         dirs.sort()
         sub_dirs = []
-        for sub_dir in sub_dirs:
+        for sub_dir in dirs:
             if path.isdir(directory + "/" + sub_dir):
                 sub_dirs.append(sub_dir)
         return sub_dirs
