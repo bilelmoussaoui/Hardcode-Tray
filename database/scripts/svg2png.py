2bce0cdb69ddfe99e4702f412ca1d30a7fdd4c10
bil-elmoussaoui <bil.elmoussaoui@gmail.com>
2015-08-20 08:34:11 +0200

    remove unneeded slash

--- database/scripts/svg2png.py
+++ database/scripts/svg2png.py
@@ -6,8 +6,8 @@ import sys
 origfile = sys.argv[1]
 symlfile = sys.argv[2]
 
-with open(origfile, 'r') as content_file:
+with open(origfile, "r") as content_file:
     svg = content_file.read()
-fout = open(symlfile, 'wb')
-svg2png(bytestring=bytes(svg, 'UTF-8'), write_to=fout)
+fout = open(symlfile, "wb")
+svg2png(bytestring=bytes(svg, "UTF-8"), write_to=fout)
 fout.close()
