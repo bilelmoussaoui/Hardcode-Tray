bd0d4f5c33579e0608b34577ca6cd8e52d5f9f3f
bil-elmoussaoui <bil.elmoussaoui@gmail.com>
2015-10-11 21:42:31 +0200

    remove unneeded import

--- script.py
+++ script.py
@@ -13,7 +13,7 @@ from gi import require_version
 require_version('Gtk','3.0')
 from gi.repository import Gtk, Gio
 from os import environ, geteuid, getlogin, listdir, path, makedirs, chown,\
-    getenv, symlink, remove, fchown
+    getenv, symlink, remove
 from subprocess import Popen, PIPE
 from sys import exit
 from shutil import rmtree, copyfile, move
