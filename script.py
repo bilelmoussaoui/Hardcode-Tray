f8c9c42c93e53a5b973df5fee7cfa3d01628cf1f
bil-elmoussaoui <bil.elmoussaoui@gmail.com>
2015-10-13 18:45:06 +0200

    create hexchat icons folder only when it doesn't exists

--- script.py
+++ script.py
@@ -234,7 +234,7 @@ def create_hexchat_dir(apps_infos):
     icons_dir = app_path[len(app_path) - 1]
     del app_path[len(app_path) - 1]
     app_path = "/" + "/".join(app_path) + "/"
-    if path.isdir(app_path):
+    if not path.exists(app_path):
         create_dir(app_path + icons_dir + "/")
 
 
