594712b0b8370dbeb11da52c2e1686142783fbfd
bil-elmoussaoui <bil.elmoussaoui@gmail.com>
2015-10-18 11:15:44 +0200

    start using Inkscape too

--- script.py
+++ script.py
@@ -18,10 +18,9 @@ from hashlib import md5
 from collections import OrderedDict
 try:
     from cairosvg import svg2png
-    from cairocffi import cairo_version
+    use_inkscape = False
 except ImportError:
-    exit("You need to install python3-cairosvg and python3-cairocffi to run the script.\
-        \nPlease install it and try again. Exiting.")
+    use_inkscape = True
 
 from gi import require_version
 require_version('Gtk', '3.0')
@@ -87,6 +86,22 @@ def get_subdirs(directory):
         return sub_dirs
     else:
         return None
+
+
+def svgtopng(file_path, dest_path):
+    """
+        Converts svg files to png using Cairosvg or Inkscape
+        @file_path : String; the svg file absolute path
+        @dest_path : String; the png file absolute path
+    """
+    if use_inkscape:
+        execute(["inkscape", "-f" ,file_path, "-e " ,dest_path])
+    else:
+        with open(file_path, "r") as content_file:
+            svg = content_file.read()
+        fout = open(dest_path, "wb")
+        svg2png(bytestring=bytes(svg, "UTF-8"), write_to=fout)
+        fout.close()
 
 
 def mchown(directory):
@@ -437,12 +452,7 @@ def install():
                                 fixed_icons.append(fbase)
                         elif ext_theme == "svg" and ext_orig == "png":
                             try:  # Convert the svg file to a png one
-                                with open(fname, "r") as content_file:
-                                    svg = content_file.read()
-                                fout = open(output_icon, "wb")
-                                svg2png(bytestring=bytes(svg, "UTF-8"),
-                                        write_to=fout)
-                                fout.close()
+                                svgtopng(fname, output_icon)
                                 mchown(output_icon)
                             except:
                                 print("The svg file `%s` is invalid." % fname)
