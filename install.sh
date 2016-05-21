c57ad406b119b5579484a3af22b3a2e7027c4973
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2016-05-22 01:45:32 +0200

    Latest fix to install script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
+#!/bin/bash
 cd /tmp
 echo "Downloading Hardcode-Tray ..."
 version=$(git ls-remote -t https://github.com/bil-elmoussaoui/Hardcode-Tray.git | awk '{print $2}' | cut -d '/' -f 3 | cut -d '^' -f 1  | sort -b -t . -k 1,1nr -k 2,2nr -k 3,3r -k 4,4r -k 5,5r | uniq)
 wget -q https://github.com/bil-elmoussaoui/Hardcode-Tray/archive/$version.zip
 unzip -oq $version.zip
 rm -f $version.zip
-echo $version
 versionnb=$(echo "$version" | sed "s/[^0-9. ]*//")
 cd ./Hardcode-Tray-$versionnb
 rm -rf .git ./screenshots ./license
