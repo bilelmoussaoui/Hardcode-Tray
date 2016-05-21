0f65239662b6a7edd86e5b6f439cdf3928b6c1f2
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2016-05-22 01:31:14 +0200

    Install/update to the latest release

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 cd /tmp
 echo "Downloading Hardcode-Tray ..."
-wget -q https://github.com/bil-elmoussaoui/Hardcode-Tray/archive/master.zip
-unzip -oq master.zip
-rm -f master.zip
-cd ./Hardcode-Tray-master
+version=$(git ls-remote -t https://github.com/bil-elmoussaoui/Hardcode-Tray.git | awk '{print $2}' | cut -d '/' -f 3 | cut -d '^' -f 1  | sort -b -t . -k 1,1nr -k 2,2nr -k 3,3r -k 4,4r -k 5,5r | uniq)
+wget -q https://github.com/bil-elmoussaoui/Hardcode-Tray/archive/$version.zip
+unzip -oq $version.zip
+rm -f $version.zip
+versionnb=$(version | sed "s/[^0-9. ]*//g")
+cd ./Hardcode-Tray-$versionnb
 rm -rf .git ./screenshots ./license
 rm -f .gitignore README.md install.sh uninstall.sh
 cd ../
 if [ -d "/opt/Hardcode-Tray" ];then
     sudo rm -rf /opt/Hardcode-Tray
 fi
-sudo mv Hardcode-Tray-master/ /opt/Hardcode-Tray
+sudo mv Hardcode-Tray-$versionnb/ /opt/Hardcode-Tray
 echo "Creating symbolic link.."
 if [ -L "/usr/bin/hardcode-tray" ];then
     sudo rm -f /usr/bin/hardcode-tray
