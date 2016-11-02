c845ff47a6b8358956edd5919e596d2714953e15
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2016-11-02 13:12:56 +0100

    bump version number

--- script.py
+++ script.py
@@ -2,7 +2,7 @@
 """
 Author : Bilal Elmoussaoui (bil.elmoussaoui@gmail.com)
 Contributors : Andreas Angerer, Joshua Fogg
-Version : 3.2
+Version : 3.3
 Website : https://github.com/bil-elmoussaoui/Hardcode-Tray
 Licence : The script is released under GPL,
         uses some icons and a modified script form Chromium project
