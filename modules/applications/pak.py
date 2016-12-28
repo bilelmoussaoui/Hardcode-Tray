92bce7fc5cf23169dfe40b3a72d6f1a64a729500
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2016-12-28 19:17:33 +0100

    add modules folder

---
+++ modules/applications/pak.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"""
+Fixes Hardcoded tray icons in Linux.
+
+Author : Bilal Elmoussaoui (bil.elmoussaoui@gmail.com)
+Contributors : Andreas Angerer, Joshua Fogg
+Version : 3.6
+Website : https://github.com/bil-elmoussaoui/Hardcode-Tray
+Licence : The script is released under GPL, uses a modified script
+     form Chromium project released under BSD license
+This file is part of Hardcode-Tray.
+Hardcode-Tray is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+TwoFactorAuth is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with Hardcode-Tray. If not, see <http://www.gnu.org/licenses/>.
+"""
+from os import path
+from imp import load_source
+from modules.applications.application import Application
+from modules.utils import backup, revert
+
+absolute_path = path.split(path.abspath(__file__))[0] + "/../"
+data_pack = load_source('data_pack', absolute_path + 'data_pack.py')
+
+
+class PakApplication(Application):
+    """Pak Application class, based on data_pak file."""
+
+    def __init__(self, application_data, svgtopng):
+        Application.__init__(self, application_data, svgtopng)
+
+    def backup_binary(self, icon_path):
+        """Backup binary file before modification."""
+        backup(icon_path + self.app.data["binary"])
+
+    def revert_binary(self, icon_path):
+        """Restore the backed up binary file."""
+        revert(icon_path + self.app.data["binary"])
+
+    def reinstall(self):
+        """Reinstall the old icons."""
+        for icon_path in self.get_icons_path():
+            self.revert_binary(icon_path)
+
+    def install(self):
+        """Install the application icons."""
+        self.install_symlinks()
+        for icon_path in self.get_icons_path():
+            self.backup_binary(icon_path)
+            for icon in self.get_icons():
+                self.install_icon(icon, icon_path)
+
+    def install_icon(self, icon, icon_path):
+        """Install the icon."""
+        filename = icon_path + self.app.data["binary"]
+        icon_to_repl = icon["original"]
+        icon_for_repl = icon["theme"]
+        icon_extension = icon["orig_ext"]
+        if self.svgtopng.is_svg_enabled():
+            if icon_extension == '.svg':
+                pngbytes = self.svgtopng.to_bin(icon_for_repl)
+            else:
+                with open(icon_for_repl, 'rb') as pngfile:
+                    pngbytes = pngfile.read()
+
+            _data_pack = data_pack.read_data_pack(filename)
+            _data_pack.resources[int(icon_to_repl)] = pngbytes
+            data_pack.write_data_pack(_data_pack.resources, filename, 0)
---
+++ modules/data.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python3
+"""
+Fixes Hardcoded tray icons in Linux.
+
+Author : Bilal Elmoussaoui (bil.elmoussaoui@gmail.com)
+Contributors : Andreas Angerer, Joshua Fogg
+Version : 3.6
+Website : https://github.com/bil-elmoussaoui/Hardcode-Tray
+Licence : The script is released under GPL, uses a modified script
+     form Chromium project released under BSD license
+This file is part of Hardcode-Tray.
+Hardcode-Tray is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+TwoFactorAuth is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with Hardcode-Tray. If not, see <http://www.gnu.org/licenses/>.
+"""
+from os import path
+import json
+from modules.utils import create_dir, get_iterated_icons
+from modules.const import USERHOME, ARCH
+from modules.icon import Icon
+
+
+class DataManager:
+
+    def __init__(self, database_file, theme,
+                 default_icon_size, custom_path=""):
+        self.default_icon_size = default_icon_size
+        self.json_file = database_file
+        self.custom_path = custom_path
+        self.theme = theme
+        self.dont_install = True
+        self.data = {}
+        self.supported_icons_cnt = 0
+        self.read()
+
+    def get_type(self):
+        """Get the type of database(Application) file."""
+        if self.data["is_script"]:
+            return self.data["script"]
+        elif self.data["is_qt"]:
+            return "qt"
+        else:
+            return "normal"
+
+    def get_icons(self):
+        """Return the application icons."""
+        return self.data["icons"]
+
+    def get_theme(self, is_dark):
+        """Get the theme to be used dark or light."""
+        if isinstance(self.theme, list):
+            if is_dark:
+                theme = self.theme[1]
+            else:
+                theme = self.theme[0]
+        else:
+            theme = self.theme
+        return theme
+
+    def is_installed(self):
+        """Check whether the application is installed or not."""
+        return not self.dont_install
+
+    def read(self):
+        """Read json file in order to apply the script later."""
+        if path.isfile(self.json_file):
+            with open(self.json_file) as data_file:
+                self.data = json.load(data_file)
+            self.check_paths()
+            be_added = len(self.data["app_path"]) > 0
+            if self.custom_path:
+                if self.is_only and path.exists(self.custom_path):
+                    self.data["app_path"].append(self.custom_path)
+            if be_added:
+                self.dont_install = False
+                if isinstance(self.data["icons"], list):
+                    self.data["icons"] = get_iterated_icons(self.data["icons"])
+                self.get_app_icons()
+
+    def get_app_icons(self):
+        """Get a list of icons of each application."""
+        if self.is_installed():
+            icons = self.get_icons()
+            supported_icons = []
+            for icon_data in icons:
+                if isinstance(icons, list):
+                    icon = Icon(icon_data, self.theme, self.default_icon_size)
+                else:
+                    icon = Icon(icons[icon_data], self.theme,
+                                self.default_icon_size)
+                if icon.get_is_exists():
+                    supported_icons.append(icon.get_data())
+                    self.supported_icons_cnt += 1
+
+            self.dont_install = not len(supported_icons) > 0
+            self.data["icons"] = supported_icons
+
+    def check_paths(self):
+        """
+        Check if the app_path exists to detect if the application is installed.
+
+        Also check if the icons path exists, and force creating needed folders.
+        See the json key "force_create_folder"
+        """
+        self.data["app_path"] = list(map(
+            self.replace_vars_path, self.data["app_path"]))
+        self.data["icons_path"] = list(map(
+            self.replace_vars_path, self.data["icons_path"]))
+        new_app_path = []
+        for app_path in self.data["app_path"]:
+            if path.isdir(app_path) or path.isfile(app_path):
+                new_app_path.append(app_path)
+        self.data["app_path"] = new_app_path
+        if not len(self.data["app_path"]) == 0:
+            new_icons_path = []
+            for icon_path in self.data["icons_path"]:
+                if self.data["force_create_folder"]:
+                    create_dir(icon_path)
+                if path.isdir(icon_path):
+                    new_icons_path.append(icon_path)
+            self.data["icons_path"] = new_icons_path
+
+    def replace_vars_path(self, _path):
+        """Replace common variables informations."""
+        _path = _path.replace("{userhome}", USERHOME)
+        _path = _path.replace("{arch}", ARCH)
+        return _path
