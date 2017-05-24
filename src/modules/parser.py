7598bd5e8e3c7c1ce2c33cd312cb361146bf2e44
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2017-05-24 13:08:29 +0200

    Update parser.py

--- src/modules/parser.py
+++ src/modules/parser.py
@@ -96,7 +96,8 @@ class Parser:
     def _parse_paths(self, paths, key):
         for path in paths:
             path = Path(path, self, key)
-            if path.exists:  # If path exists
+            # Check if path exists
+            if path.exists or (self.force_create_folder and key == "icons_path"):
                 getattr(self, key).append(path)
 
     def _parse_icons(self, icons):
