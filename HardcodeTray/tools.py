6de5fa9562307de0c693466e698f3a641353b736
Térence Clastres <t.clastres@gmail.com>
2019-08-29 22:40:37 +0200

    Add gnome-xorg to the DE list (#594)
    $DESKTOP_SESSION can report gnome as gnome-xorg

--- HardcodeTray/tools.py
+++ HardcodeTray/tools.py
@@ -35,6 +35,9 @@ def detect_de(de_list):
         if desktop in de_list:
             Logger.debug("DE: {0}".format(desktop.title()))
             return desktop
+        elif desktop == "gnome-xorg":
+            Logger.debug("DE: {0} (falling back to gnome)".format(desktop.title()))
+            return "gnome"
     Logger.debug("DE not detected.")
     return "other"
 
