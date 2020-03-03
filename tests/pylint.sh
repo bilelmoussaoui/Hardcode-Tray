cdd2ff1257f13a3d066e19c2b26fca7b886dad04
Sergei Eremenko <finalitik@gmail.com>
2020-03-03 22:55:46 +0200

    Fix `pip3: command not found` error in tests

--- tests/pylint.sh
+++ tests/pylint.sh
@@ -1,3 +1,3 @@
-#/usr/bin env sh
+#!/usr/bin/env sh
 SITE_PACKAGES=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
-find $SITE_PACKAGES/HardcodeTray/ -name "*.py" | xargs pylint --rcfile=.pylintrc $1
+find "$SITE_PACKAGES"/HardcodeTray/ -name '*.py' | xargs pylint --rcfile=.pylintrc $1
